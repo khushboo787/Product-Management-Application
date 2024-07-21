71bb6aff09132741ef535bbf072f1b994aa7bc0a
Khushboo Kumari <115460837+khushboo787@users.noreply.github.com>
2024-07-21 13:49:04 +0530

    Update ProductManagementSystemApplication.java

--- Product-Management-System/src/main/java/com/myapp/ProductManagementSystemApplication.java
+++ Product-Management-System/src/main/java/com/myapp/ProductManagementSystemApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductManagementSystemApplication {
+       // Access the PORT environment variable
+       String port = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManagementSystemApplication.class, args);
